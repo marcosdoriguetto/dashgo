c190277d4298f6c793a75619b36cf05c45927065
Marcos <marcosdoriguetto@gmail.com>
2022-08-04 14:29:32 -0300

    feat: create Users page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from "@chakra-ui/react";
+import { RiAddLine as AddIcon, RiPencilLine as EditIcon } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Users
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={AddIcon} fontSize="16" />}
+            >
+              Create new
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Users</Th>
+                <Th>Registration date</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text>Marcos Fernandes</Text>
+                    <Text>marcosdoriguetto@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>16 de Dezembro, 2022</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blackAlpha"
+                    leftIcon={<Icon as={EditIcon} />}
+                  >
+                    Edit
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
