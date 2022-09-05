d37cbe7fecd9bf5e2dd5803bb745ead26af36d47
Marcos <marcosdoriguetto@gmail.com>
2022-09-05 17:47:49 -0300

    feat: add and configure mirage and react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,9 +13,12 @@ import {
   Tr,
   Text,
   useBreakpointValue,
-  IconButton,
+  Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+
 import { RiAddLine as AddIcon, RiPencilLine as EditIcon } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -23,10 +26,19 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("user-list", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  useEffect(() => {}, []);
 
   return (
     <Box>
@@ -54,48 +66,60 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Users</Th>
-                {isWideVersion && <Th>Registration date</Th>}
-                {isWideVersion && <Th></Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text>Marcos Fernandes</Text>
-                    <Text>marcosdoriguetto@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>16 de Dezembro, 2022</Td>}
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Fail to loading data</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Users</Th>
+                    {isWideVersion && <Th>Registration date</Th>}
+                    {isWideVersion && <Th></Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text>Marcos Fernandes</Text>
+                        <Text>marcosdoriguetto@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>16 de Dezembro, 2022</Td>}
 
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="blackAlpha"
-                      leftIcon={<Icon as={EditIcon} />}
-                    >
-                      Edit
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
+                    {isWideVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="sm"
+                          colorScheme="blackAlpha"
+                          leftIcon={<Icon as={EditIcon} />}
+                        >
+                          Edit
+                        </Button>
+                      </Td>
+                    )}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
